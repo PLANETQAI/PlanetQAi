d2e0fd09649a65da3dd8f13dd19f99ffcb67a2fe
Nikhil Sharma <nikhil@Nikhils-MacBook-Air.local>
2024-12-15 13:47:28 +0530

    testing

--- middleware.js
+++ middleware.js
@@ -1,1 +1,12 @@
 export { auth as middleware } from '@/auth'
+import { NextResponse } from 'next/server'
+
+import { auth } from '@/auth'
+
+export default auth(async function middleware(request) {
+	return NextResponse.next()
+})
+
+export const config = {
+	matcher: '/((?!api/auth|auth|images|_next/static|_next/image|favicon.ico|^/$).+)',
+}
