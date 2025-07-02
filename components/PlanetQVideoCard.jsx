7bf56ce8a3cf958dc22dc9f74b61eafc68d3406d
TijwaLtd <achiandoomollo64@gmail.com>
2025-07-02 14:55:38 +0300

    refactor: replace video element with Image component and update image formats

--- components/PlanetQVideoCard.jsx
+++ components/PlanetQVideoCard.jsx
@@ -1,5 +1,7 @@
 "use client";
 
+import Image from "next/image";
+
 export default function PlanetQVideoCard({ onClick }) {
   return (
     <div className="p-1 block" onClick={onClick}>
@@ -9,18 +11,24 @@ export default function PlanetQVideoCard({ onClick }) {
         <div className="text-[#afafaf] text-xs sm:text-sm md:text-lg font-semibold p-1 sm:p-2 mb-1 sm:mb-2 text-center group-hover:animate-vibrate">
           <h1 className="text-xl">Planet Q Video</h1>
         </div>
-        <div className="relative w-full" style={{ paddingBottom: "56.25%" }}>
-          <video
+        <div className="relative w-full">
+          {/* <video
             className="absolute top-0 left-0 w-full h-full rounded-lg"
             controls
             poster="/images/client.png"
           >
             <source src="/images/bg-video-compressed.mp4" type="video/mp4" />
             Your browser does not support the video tag.
-          </video>
-        </div>
-        <div className="mt-2 text-center text-sm text-gray-300">
-          Featuring futuristic music videos and content
+          </video> */}
+        
+            <Image
+              src="/images/video-player1.jpg"
+              alt="Planet Q Games"
+              width={300}
+              height={200}
+              className="w-full  rounded-lg"
+            />
+                
         </div>
       </div>
     </div>
